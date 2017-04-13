03c0c23c3cc484944eb6551bf945d4d33ea2208f
Ofer.Spivak <ofer.spivak@timetoknow.com>
2017-04-13 10:07:35 +0300

    first step - adding bomb point

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import engine.BaseGameEngine;
+import engine.settings.Settings;
+import engine.settings.SettingsImpl;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+
+        Settings settings = new SettingsImpl("player1", "player2", 5);
+        BaseGameEngine gameEngine = new BaseGameEngine(settings);
+        gameEngine.bombPoint(0,0); //hit
+        gameEngine.bombPoint(0, 1); //hit
+        gameEngine.bombPoint(1, 0); //miss
+        gameEngine.bombPoint(2,2); //hit
+
     }
 }
--- src/engine/BaseGameEngine.java
+++ src/engine/BaseGameEngine.java
@@ -1,5 +1,8 @@
 package engine;
 
+import engine.battleShip.BattleShip;
+import engine.battleShip.BattleShipImpl;
+import engine.battleShip.Direction;
 import engine.player.Player;
 import engine.player.PlayerImpl;
 import engine.settings.Settings;
@@ -12,9 +15,55 @@ public class BaseGameEngine {
     Player opponentPlayer;
 
     public BaseGameEngine(Settings settings) {
+        initGame(settings);
+    }
+
+    private void initGame(Settings settings) {
+        // create players
         currentPlayer = new PlayerImpl(settings.getPlayer1Name(), settings.getBoardSize());
-        opponentPlayer= new PlayerImpl(settings.getPlayer2Name(), settings.getBoardSize());
+        opponentPlayer = new PlayerImpl(settings.getPlayer2Name(), settings.getBoardSize());
 
+        //adding ships - todo: extract to method with validation etc...
+        BattleShip ship = new BattleShipImpl(4, Direction.ROW, 0, 0);
+        opponentPlayer.addShip(ship);
+        ship = new BattleShipImpl(2, Direction.COLUMN, 3, 1);
+        opponentPlayer.addShip(ship);
 
+        ship = new BattleShipImpl(2, Direction.COLUMN, 3, 1);
+        currentPlayer.addShip(ship);
+        ship = new BattleShipImpl(4, Direction.ROW, 2, 2);
+        currentPlayer.addShip(ship);
+    }
+
+    private void switchPlayers() {
+        Player tmp = currentPlayer;
+        currentPlayer = opponentPlayer;
+        opponentPlayer = tmp;
+    }
+
+    public void bombPoint(int x, int y) {
+        HitBoardType hit = opponentPlayer.tryToHitMyShip(x, y);
+        switch (hit) {
+            case HIT: {
+                currentPlayer.markHit(x, y);
+                break;
+            }
+            case MISS: {
+                currentPlayer.markMiss(x, y);
+                switchPlayers();
+                break;
+            }
+            case ALREADY_HIT: {
+                break;
+            }
+        }
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public Player getOpponentPlayer() {
+        return opponentPlayer;
     }
 }
